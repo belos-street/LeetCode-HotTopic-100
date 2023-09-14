54f007759c0a25bda3817f389c02596197064e92
zhi.jiang <zhi.jiang@sectrend.com.cn>
2023-09-14 14:37:07 +0800

    edit config

--- src/two-sum/index.ts
+++ src/two-sum/index.ts
@@ -1,1 +1,12 @@
-export function twoSum() {}
+export function twoSum(nums: number[], target: number) {
+  const numsMap = new Map<number, number>()
+  for (let index = 0; index < nums.length; index++) {
+    const key = target - nums[index]
+    if (numsMap.has(key)) {
+      return [numsMap.get(key), index]
+    } else {
+      numsMap.set(nums[index], index)
+    }
+  }
+  return []
+}
