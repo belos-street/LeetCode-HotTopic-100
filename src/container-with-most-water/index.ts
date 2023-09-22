e593d027b90e24c5b98d7189ba1a38f1beb40aa5
aa1213889 <869135421@qq.com>
2023-09-23 00:17:33 +0800

    feat: 实现该算法

--- src/container-with-most-water/index.ts
+++ src/container-with-most-water/index.ts
@@ -1,15 +1,12 @@
-/** 双指针 前后 一次移动一个 记录面积 直至相等  */
 export function maxArea(height: number[]): number {
-  if (height.length < 2) return 0
   let leftPoint = 0,
     rightPoint = height.length - 1
 
   let maxAreaNum = 0
   while (leftPoint < rightPoint) {
-    // /** 记录面积 */
-    // areas.push(Math.min(height[leftPoint], height[rightPoint]) * (rightPoint - leftPoint))
-    // /** 每次循环左右指针各移动一次 */
-    // leftPoint % 2 === 0 ? leftPoint++ : rightPoint--
+    const area = Math.min(height[leftPoint], height[rightPoint]) * (rightPoint - leftPoint)
+    if (maxAreaNum < area) maxAreaNum = area
+    height[leftPoint] < height[rightPoint] ? leftPoint++ : rightPoint--
   }
 
   return maxAreaNum
